2625484949c0579cd4d2eb95640be50697ffe80c
Teresa <tromerolozano@gmail.com>
2021-03-31 12:16:11 +0200

    fix files for deploy

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.set('views', path.join(__dirname + '/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use('/', rootRouter)
