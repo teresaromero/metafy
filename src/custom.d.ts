e04db009bf3696b38c707555bbd6636ceb3f0006
Teresa <hello@teresaromero.dev>
2021-04-04 13:36:24 +0200

    added custom typing (#5)

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -7,3 +7,39 @@ declare namespace Express {
     expires_in?: number
   }
 }
+
+declare module 'passport-spotify' {
+  export interface Profile {
+    provider: string
+    id: string
+    username: string
+    displayName: string
+    profileUrl: string | null
+    photos: [string] | null
+    country: string
+    followers: number | null
+    product: string | null
+    emails?: [{ value: string; type: null }]
+  }
+
+  export type VerifyCallback = (
+    error?: Error | null,
+    user?: Express.User,
+    info?: object
+  ) => void
+
+  export type VerifyFunction = (
+    accessToken: string,
+    refreshToken: string,
+    expires_in: number,
+    profile: Profile,
+    done: VerifyCallback
+  ) => void
+
+  export class Strategy {
+    constructor(options: StrategyOptions, verify: VerifyFunction)
+
+    name: string
+    authenticate(req: Express.Request, options?: object): void
+  }
+}
