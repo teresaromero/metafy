30a825751215e812d96759367c51ce404bfc88cc
Teresa <hello@teresaromero.dev>
2021-04-07 19:49:53 +0200

    fix hooks for check test and fix lint

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    verbose: true,
-    bail: true,
-    collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.{ts}"],
-    testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"]
+  preset: 'ts-jest',
+  verbose: true,
+  bail: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts}'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/']
 }
 export default config
